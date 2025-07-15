3d06a4d0b38d1f34ee53e1689361d0bf85b0dce4
Kittatach.M <popzzxc@gmail.com>
2025-07-15 20:03:38 +0700

    jwt

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+require("dotenv").config();
+console.log("Loaded SECRET:", process.env.SECRET);
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+
 const usersRouter = require("./user");
 const postRouter = require("./post");
 
@@ -12,6 +22,58 @@ app.use("/api/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Home Page");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json({ message: "User register" });
+  } catch (err) {
+    res.status(400).json({ error: "Email allready exists" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return res.status(401).json({ error: "Invalid email" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ error: "Invalid password" });
+
+  const token = jwt.sign({ userId: user.id }, SECRET, { expiresIn: "1h" });
+  res.json({ token });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.user = decoded;
+    next();
+  });
+}
+
+app.get("/profile", authenticateToken, async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.user.userId },
+    select: { id: true, name: true, email: true },
+  });
+  res.json(user);
 });
 
 app.listen(port, () => {
